f9817eced04cb51c521bcb9ed9bdf5721a7fcf53
NingNan <nanning9925@gmail.com>
2018-05-26 19:38:05 +1000

    Complete

--- src/view/GameEngineCallbackGUI.java
+++ src/view/GameEngineCallbackGUI.java
@@ -1,7 +1,13 @@
 package view;
 
+import java.util.logging.Level;
+
 import javax.swing.SwingUtilities;
 
+import com.sun.security.auth.NTDomainPrincipal;
+import com.sun.xml.internal.ws.policy.EffectiveAlternativeSelector;
+
+import controller.HouseRollListener;
 import model.interfaces.DicePair;
 import model.interfaces.GameEngine;
 import model.interfaces.GameEngineCallback;
@@ -41,13 +47,50 @@ public class GameEngineCallbackGUI implements GameEngineCallback {
 
 	@Override
 	public void houseResult(DicePair result, GameEngine gameEngine) {
-		String message = getMessage(null, result, "*RESULT*");
-		
-		
-		roll(message);
 
+		
+		String message = "<html>" + getMessage(null, result, "*RESULT*") + "<br>";
+		
+		int houseSum = result.getDice1() + result.getDice2();
+		
+		for (Player player: gameEngine.getAllPlayers()){
+			
+			int playerSum = player.getRollResult().getDice1() + player.getRollResult().getDice2();
+			
+			if (playerSum > houseSum)
+			{
+				message = message + "Win! ";
+			}
+			else if (playerSum == houseSum)
+			{
+				message = message + "Draw! ";
+			}
+			else if (playerSum < houseSum)
+			{
+				message = message + "Lose! ";
+			}
+			
+			message = message + player.toString() + ", Result Sum: " + playerSum +  "<br>"; 
+			
+			player.setRollResult(null);
+		}
+		
+		final String resultMessage = message + "<br>Note: This round is end and the player dice result is reset!" + "</html>";
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				
+				ui.updatePanel(resultMessage);
+				ui.updateStatus(ui.getSelectPlayer());
+			}
+		});
 	}
 
+	
+	
+	
+	
 	private String getMessage(Player player, DicePair dicePair, String action) {
 		// house player
 		if (player == null) {
@@ -68,6 +111,7 @@ public class GameEngineCallbackGUI implements GameEngineCallback {
 		}
 	}
 	
+	
 	private void roll(String str){
 		
 		final String message = str;
@@ -77,6 +121,7 @@ public class GameEngineCallbackGUI implements GameEngineCallback {
 			public void run() {
 				
 				ui.updatePanel(message);
+				ui.updateStatus(ui.getSelectPlayer());
 			}
 		});
 	}
